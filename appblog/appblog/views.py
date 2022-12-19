9c59fb1792f612c4e755d2191f586af0170e8421
unknown <marcos_g91@hotmail.com>
2022-12-19 19:21:22 -0300

    reemplazo de archivos

--- appblog/appblog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render 
-
-def inicio(request):
-    template_name = "publicaciones/index.html"
-    contexto = {
-        
-    }
-    return render( request, template_name, contexto)
---
+++ blog/Articulos/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from Articulos.models import Entrada
+# Register your models here.
+admin.site.register(Entrada)
